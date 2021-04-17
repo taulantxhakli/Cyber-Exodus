5a68fddbc65af42e40aa0bc975cec4c53afa58f2
unknown <txhakli17@stac.edu>
2021-04-17 14:58:10 -0400

    enemy update

---
+++ Assets/personal assets/scripts/deathController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+using UnityEngine.VFX;
+using Random = UnityEngine.Random;
+
+namespace MikeNspired.UnityXRHandPoser {
+
+    public class deathController : MonoBehaviour
+    {
+        public GameObject Mesh;
+        public UnityEventFloat onHit;
+        
+        // Start is called before the first frame update
+        void Start()
+        {
+            setRigidbodyState(true);
+            setColliderState(false);
+        }
+
+        public void die(float damage, GameObject damager) {
+            onHit.Invoke(damage);
+            GetComponent<Animator>().enabled = false;
+            setRigidbodyState(false);
+            setColliderState(true);
+
+            Destroy(gameObject, 3f); // remove npc from scene after 3s
+        }
+
+        void setRigidbodyState(bool state) {
+            Rigidbody[] rigidbodies = GetComponentsInChildren<Rigidbody>();
+
+            foreach(Rigidbody rigidbody in rigidbodies) {
+                rigidbody.isKinematic = state;
+            }
+
+            //GetComponent<Rigidbody>().isKinematic = !state;
+        }
+
+        void setColliderState(bool state) {
+            Collider[] colliders = GetComponentsInChildren<Collider>();
+
+            foreach(Collider collider in colliders) {
+                collider.enabled = state;
+            }
+
+            //GetComponent<Collider>().enabled = !state;
+
+        }
+    }
+}
