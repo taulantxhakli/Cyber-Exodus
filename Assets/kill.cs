3170fba727d5d0cd9321f0ab46930b40e3d109e1
unknown <txhakli17@stac.edu>
2021-04-17 13:53:42 -0400

    enemy update

---
+++ Assets/kill.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+using UnityEngine.VFX;
+using Random = UnityEngine.Random;
+
+
+namespace MikeNspired.UnityXRHandPoser
+{
+    public class kill : MonoBehaviour
+    {
+
+        public ParticleSystem particleSystemSplash;
+        public GameObject Mesh;
+        AudioSource m_AudioSource;
+        public UnityEventFloat onHit;
+
+        void OnEnable()
+        {
+            if (particleSystemSplash)
+                particleSystemSplash.Stop();
+        }
+
+        public void TakeDamage(float damage, GameObject damager)
+        {
+            onHit.Invoke(damage);
+            //GetComponent<AudioRandomize>().PlaySound();
+            particleSystemSplash.transform.parent = null;
+            particleSystemSplash.gameObject.SetActive(true);
+            particleSystemSplash.Play();
+            Mesh.SetActive(false);
+
+            Transform camera = Camera.main.transform;
+            var position = transform.position - camera.position;
+
+            Destroy(gameObject, 3);
+        }
+    }
+
+}
